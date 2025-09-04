987c7c4883ab847b019224900a028a7ab65fd1b2
LaisZagati <laisviana97@gmail.com>
2025-09-04 12:15:59 -0400

    API Integration

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 75,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  //Make a request to the SheCodes AI API
+  let userInstructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f093ocaff400a6043tff45112437b840";
+  let prompt = `Generate a funny joke about any subject requested ${userInstructionsInput.value}`;
+  let context =
+    "You are an expert in writing funny jokes about anything and everything.Your mission is to write short and funny jokes about topic requested. Be creative and do not repeat yourself too much.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  // Display the generated poem
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.getElementById("poem-topic-form");
